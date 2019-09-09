b577edc6b7a03e0e6a9aeb8802c774496a5d1adb
war4vick <55099294+war4vick@users.noreply.github.com>
2019-09-09 18:24:51 +0300

    beta

---
+++ copy.py
@@ -0,0 +1,137 @@
+import os,mmap,time,struct,signal,sys,subprocess,logging,whisper,optparse
+from optparse import OptionParser
+import numpy as np
+from graphyte import *
+from multiprocessing import Process
+
+
+option_parser = optparse.OptionParser(usage='''%prog path to dir -s localhost -p 2003 -u udp -e .wsp -d whisper''')
+option_parser.add_option('-s', '--server', default='localhost', help='hostname of server to send  default:"localhost"')
+option_parser.add_option('-p', '--port', type=int, default=2003 ,help='port to send message to default:"2003"')
+option_parser.add_option('-o', '--protocol', action='store_true',default='udp', help='send via UDP instead of TCP default:"tcp"')
+option_parser.add_option('-e', '--db_exp', default='.wsp', action='store_true',help='database expansion default:".wsp"')
+option_parser.add_option('-d', '--db_name', default='whisper', action='store',type='string', help='Whisper database dir name default:"whisper"')
+option_parser.add_option( '--debug', default=False, action='store_true', help='debug')
+(options, args) = option_parser.parse_args()
+
+if options.debug:
+    logging.basicConfig(
+        level=logging.DEBUG ,
+        format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        )
+else:
+    logging.basicConfig(
+        filename='error_log.log',
+        level=logging.ERROR ,
+        format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        )
+
+if len(args) != 1:
+  option_parser.error("require one input file name")
+else:
+  path = args[0]
+
+def mmap_file(filename):
+  fd = os.open(filename, os.O_RDONLY)
+  map = mmap.mmap(fd, os.fstat(fd).st_size, prot=mmap.PROT_READ)
+  os.close(fd)
+  return map
+
+#transformation path like /var/lib/graphite/whisper/carbon/metric/name.wsp to carbon prefix carbon.metric.name
+def read_path(path):
+  pat=path.split(os.sep)
+  lis=np.array(pat)
+  name=np.array(file)
+  #definition of all element in 'pat'
+  end=int(len(pat))
+  #search in a list by name of base and setting a start index to create a prefix
+  start=int((pat.index(options.db_name))+1)
+  path_full=""
+  #databases expansion length like ".wsp"  == 4
+  llen=-1+-len(options.db_exp)
+  #creating a metric prefix relative to the database directory
+  for x in range(start,end):
+    path_full+=(lis[x])+"."
+  #remove databases expansion with in prefix
+  return (path_full[:llen])
+
+def read_header(map):
+  try:
+    (aggregationType, maxRetention, xFilesFactor, archiveCount) \
+      = struct.unpack(whisper.metadataFormat, map[:whisper.metadataSize])
+  except (struct.error, ValueError, TypeError):
+    raise whisper.CorruptWhisperFile("Unable to unpack header")
+
+  archives = []
+  archiveOffset = whisper.metadataSize
+
+  for i in xrange(archiveCount):
+    try:
+      (offset, secondsPerPoint, points) = struct.unpack(
+        whisper.archiveInfoFormat,
+        map[archiveOffset:archiveOffset + whisper.archiveInfoSize]
+      )
+    except (struct.error, ValueError, TypeError):
+      raise whisper.CorruptWhisperFile("Unable to read archive %d metadata" % i)
+
+    archiveInfo = {
+      'offset': offset,
+      'secondsPerPoint': secondsPerPoint,
+      'points': points,
+      'retention': secondsPerPoint * points,
+      'size': points * whisper.pointSize,
+    }
+    archives.append(archiveInfo)
+    archiveOffset += whisper.archiveInfoSize
+
+  header = {
+    'aggregationMethod': whisper.aggregationTypeToMethod.get(aggregationType, 'average'),
+    'maxRetention': maxRetention,
+    'xFilesFactor': xFilesFactor,
+    'archives': archives,
+  }
+  return header
+
+def dump_archives(archives,path):
+    for i, archive in enumerate(archives):
+        offset = archive['offset']
+        for point in xrange(archive['points']):
+            (timestamp, value) = struct.unpack(whisper.pointFormat,map[offset:offset + whisper.pointSize])
+        if  timestamp != 0:
+            prefix=read_path(path)
+            logging.debug('Read prefix:%s'%(prefix)+' Value:%d'%(value,))
+            try:
+                sender = Sender(options.server,protocol=options.protocol)
+                sender.send('%s'%(prefix,), int(value), timestamp=int(timestamp))
+            except IOError as e:
+                logging.error('Error occurred ' + str(e) +' Prefix:%s'%(prefix,)+' Value:%d'%(value))
+
+if __name__ == '__main__':
+    print('start')
+    time_start = time.time()
+    procs=[]
+    #find all files in dir with path
+    #root = full path to dir default:/var/lib/graphite/whisper/
+    #files = all file in all dir default:*name*.wsp
+    for root, dirs, files in os.walk(path):
+        #main cycle
+
+        for file  in files :
+            print('Write DB %s'%(file,))
+            if file.endswith(options.db_exp):
+                map = mmap_file(root+"/"+file)
+                header = read_header(map)
+                try:
+                    #run send current metrics databases to read like :/var/lib/graphite/whisper/*/*.wsp as subprocess
+                    proc = Process(target=dump_archives, args=(header['archives'],root+"/"+file))
+                    procs.append(proc)
+                    proc.start()
+
+                except IOError as e:
+                    logging.error('Error occurred ' + str(e))
+
+    print("It took "+ str(time.time()-time_start)+" seconds.")    #logging.info('Success!!!!')
+    logging.info ("It took "+ str(time.time()-time_start)+" seconds.")
+    logging.info ("List processing complete.")
---
+++ graphyte.py
@@ -0,0 +1,231 @@
+"""Send data to Graphite metrics server (synchronously or on a background thread).
+
+For example usage, see README.rst.
+
+This code is licensed under a permissive MIT license -- see LICENSE.txt.
+
+The graphyte project lives on GitHub here:
+https://github.com/Jetsetter/graphyte
+"""
+
+import atexit
+import logging
+try:
+    import queue
+except ImportError:
+    import Queue as queue  # Python 2.x compatibility
+import socket
+import threading
+import time
+
+__all__ = ['Sender', 'init', 'send']
+
+__version__ = '1.6.0'
+
+default_sender = None
+logger = logging.getLogger(__name__)
+
+
+def _has_whitespace(value):
+    return not value or value.split(None, 1)[0] != value
+
+
+class Sender:
+    def __init__(self, host, port=2003, prefix=None, timeout=5, interval=None,
+                 queue_size=None, log_sends=False, protocol='tcp', batch_size=1000):
+        """Initialize a Sender instance, starting the background thread to
+        send messages at given interval (in seconds) if "interval" is not
+        None. Send at most "batch_size" messages per socket send operation (default=1000).
+        Default protocol is TCP; use protocol='udp' for UDP.        
+        """
+        self.host = host
+        self.port = port
+        self.prefix = prefix
+        self.timeout = timeout
+        self.interval = interval
+        self.log_sends = log_sends
+        self.protocol = protocol
+        self.batch_size = batch_size
+
+        if self.interval is not None:
+            if queue_size is None:
+                queue_size = int(round(interval)) * 100
+            self._queue = queue.Queue(maxsize=queue_size)
+            self._thread = threading.Thread(target=self._thread_loop)
+            self._thread.daemon = True
+            self._thread.start()
+            atexit.register(self.stop)
+
+    def __del__(self):
+        self.stop()
+
+    def stop(self):
+        """Tell the sender thread to finish and wait for it to stop sending
+        (should be at most "timeout" seconds).
+        """
+        if self.interval is not None:
+            self._queue.put_nowait(None)
+            self._thread.join()
+            self.interval = None
+
+    def build_message(self, metric, value, timestamp, tags={}):
+        """Build a Graphite message to send and return it as a byte string."""
+        if _has_whitespace(metric):
+            raise ValueError('"metric" must not have whitespace in it')
+        if not isinstance(value, (int, float)):
+            raise TypeError('"value" must be an int or a float, not a {}'.format(
+                type(value).__name__))
+
+        tags_strs = [u';{}={}'.format(k, v) for k, v in sorted(tags.items())]
+        if any(_has_whitespace(t) for t in tags_strs):
+            raise ValueError('"tags" keys and values must not have whitespace in them')
+        tags_suffix = ''.join(tags_strs)
+
+        message = u'{}{}{} {} {}\n'.format(
+            self.prefix + '.' if self.prefix else '',
+            metric,
+            tags_suffix,
+            value,
+            int(round(timestamp))
+        )
+        message = message.encode('utf-8')
+        return message
+
+    def send(self, metric, value, timestamp=None, tags={}):
+        """Send given metric and (int or float) value to Graphite host.
+        Performs send on background thread if "interval" was specified when
+        creating this Sender.
+
+        If a "tags" dict is specified, send the tags to the Graphite host along with the metric.
+        """
+        if timestamp is None:
+            timestamp = time.time()
+        message = self.build_message(metric, value, timestamp, tags)
+
+        if self.interval is None:
+            self.send_socket(message)
+        else:
+            try:
+                self._queue.put_nowait(message)
+            except queue.Full:
+                logger.error('queue full when sending {!r}'.format(message))
+
+    def send_message(self, message):
+        if self.protocol == 'tcp':
+            sock = socket.create_connection((self.host, self.port), self.timeout)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            try:
+                sock.send(message)
+            finally:  # sockets don't support "with" statement on Python 2.x
+                sock.close()
+        elif self.protocol == 'udp':
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            try:
+                sock.sendto(message, (self.host, self.port))
+            finally:
+                sock.close()
+        else:
+            raise ValueError('"protocol" must be \'tcp\' or \'udp\', not {!r}'.format(self.protocol))
+
+    def send_socket(self, message):
+        """Low-level function to send message bytes to this Sender's socket.
+        You should usually call send() instead of this function (unless you're
+        subclassing or writing unit tests).
+        """
+        if self.log_sends:
+            start_time = time.time()
+        try:
+            self.send_message(message)
+        except Exception as error:
+            logger.error('error sending message {!r}: {}'.format(message, error))
+        else:
+            if self.log_sends:
+                elapsed_time = time.time() - start_time
+                logger.info('sent message {!r} to {}:{} in {:.03f} seconds'.format(
+                        message, self.host, self.port, elapsed_time))
+
+    def _thread_loop(self):
+        """Background thread used when Sender is in asynchronous/interval mode."""
+        last_check_time = time.time()
+        messages = []
+        while True:
+            # Get first message from queue, blocking until the next time we
+            # should be sending
+            time_since_last_check = time.time() - last_check_time
+            time_till_next_check = max(0, self.interval - time_since_last_check)
+            try:
+                message = self._queue.get(timeout=time_till_next_check)
+            except queue.Empty:
+                pass
+            else:
+                if message is None:
+                    # None is the signal to stop this background thread
+                    break
+                messages.append(message)
+
+                # Get any other messages currently on queue without blocking,
+                # paying attention to None ("stop thread" signal)
+                should_stop = False
+                while True:
+                    try:
+                        message = self._queue.get_nowait()
+                    except queue.Empty:
+                        break
+                    if message is None:
+                        should_stop = True
+                        break
+                    messages.append(message)
+                if should_stop:
+                    break
+
+            # If it's time to send, send what we've collected
+            current_time = time.time()
+            if current_time - last_check_time >= self.interval:
+                last_check_time = current_time
+                for i in range(0, len(messages), self.batch_size):
+                   batch = messages[i:i + self.batch_size]
+                   self.send_socket(b''.join(batch))
+                messages = []
+
+        # Send any final messages before exiting thread
+        for i in range(0, len(messages), self.batch_size):
+            batch = messages[i:i + self.batch_size]
+            self.send_socket(b''.join(batch))
+
+def init(*args, **kwargs):
+    """Initialize default Sender instance with given args."""
+    global default_sender
+    default_sender = Sender(*args, **kwargs)
+
+
+def send(*args, **kwargs):
+    """Send message using default Sender instance."""
+    default_sender.send(*args, **kwargs)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('metric',
+                        help='name of metric to send')
+    parser.add_argument('value', type=float,
+                        help='numeric value to send')
+    parser.add_argument('-s', '--server', default='localhost',
+                        help='hostname of Graphite server to send to, default %(default)s')
+    parser.add_argument('-p', '--port', type=int, default=2003,
+                        help='port to send message to, default %(default)d')
+    parser.add_argument('-u', '--udp', action='store_true',
+                        help='send via UDP instead of TCP')
+    parser.add_argument('-t', '--timestamp', type=int,
+                        help='Unix timestamp for message (defaults to current time)')
+    parser.add_argument('-q', '--quiet', action='store_true',
+                        help="quiet mode (don't log send to stdout)")
+    args = parser.parse_args()
+
+    if not args.quiet:
+        logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+    sender = Sender(args.server, port=args.port, log_sends=not args.quiet,
+                    protocol='udp' if args.udp else 'tcp')
+    sender.send(args.metric, args.value, timestamp=args.timestamp)
